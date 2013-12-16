7f947287637ae508d4c8bf7ec5d36b9ace5e4244
fabbychips <fabbychips@163.com>
2013-12-16 18:51:12 +0800

    添加commons httpclient lib包

--- src/com/fabby/GetUrlContent.java
+++ src/com/fabby/GetUrlContent.java
@@ -1,4 +1,14 @@
 package com.fabby;
+import java.io.UnsupportedEncodingException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.httpclient.methods.PostMethod;
 
 public class GetUrlContent {
 
